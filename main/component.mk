67589b90fb17ad28fe4faf23e49c3db3d3abfb51
surfinlemex <surfin.lemex@xelox.org>
2021-11-28 14:55:57 +0600

    Remove WiFi

--- main/component.mk
+++ main/component.mk
@@ -3,5 +3,5 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_SRCDIRS := . ili9341 ili9341/fonts adc console wifi
+COMPONENT_SRCDIRS := . ili9341 ili9341/fonts adc console
 #display display/fonts adc console
